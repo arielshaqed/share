883171e7bd849ed3e8169d96e91358f6134782d1
Ariel Shaqed (Scolnicov) <ariels@treeverse.io>
2021-05-26 14:47:11 +0300

    Change getData to hit on LGTM
    It should look more like the bad example [in the query](https://lgtm.com/rules/1510366186013/)

--- go/pkg/test/uncontrolled_data.go
+++ go/pkg/test/uncontrolled_data.go
@@ -1,18 +1,23 @@
-package test
+package main
 
 import (
 	"fmt"
+	"ioutil"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 
-func getData(req *http.Request) error {
-	p := req.URL.Query().Get("path")
+func getData(w http.ResponseWriter, req *http.Request) error {
+	p := req.URL.Query().Get("path")[0]
 
-	o, _ := os.Create("/tmp/" + p)
+	o, _ := os.Create(filepath.Join("/tmp/", p))
 	if _, err := fmt.Fprintln(o, "Duly clobbered."); err != nil {
 		return err
 	}
+
+	w.Write(ioutil.ReadFile(filepath.Join("/var/tmp/", p)))
+	
 	return o.Close()
 }
